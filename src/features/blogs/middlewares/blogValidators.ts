0c1eddbd83176c4f9b9d171edc25dd7c5c778200
Bohdan Biliak <bohdan.biliak.detrox@gmail.com>
2025-01-03 10:14:53 +0000

    V1.0

--- src/features/blogs/middlewares/blogValidators.ts
+++ src/features/blogs/middlewares/blogValidators.ts
@@ -4,21 +4,27 @@ import {NextFunction, Response, Request} from "express";
 import {blogsRepository} from "../blogsRepository";
 import {adminMiddleware} from "../../../global_middlewares/admin-middleware";
 
-export const  nameValidator =
-    body("name").isString().withMessage('not string')
-    .trim()
-        .isLength({min: 1, max:15}).withMessage("more than 15 or 0")
+export const nameValidator =
+    body("name")
+        .exists().withMessage('name is required')
+        .isString().withMessage('name must be a string')
+        .trim()
+        .isLength({ min: 1, max: 15 }).withMessage('name length should be between 1 and 15');
 
-export const descriptionValidator = body("description").isString().withMessage('not string')
-    .trim().isLength({min: 1, max:500}).withMessage('more than 500 characters long or 0')
-
+export const descriptionValidator =
+    body("description")
+        .exists().withMessage('description is required')
+        .isString().withMessage('description must be a string')
+        .trim()
+        .isLength({ min: 1, max: 500 }).withMessage('description length should be between 1 and 500');
 
 export const websiteUrlValidator =
     body("websiteUrl")
-        .isString().withMessage('websiteUrl is not a valid string')
+        .exists().withMessage('websiteUrl is required')
+        .isString().withMessage('websiteUrl must be a string')
+        .trim()
         .isLength({ min: 1, max: 100 }).withMessage('websiteUrl length should be between 1 and 100')
         .isURL().withMessage('websiteUrl is not a valid URL');
-
 
 export const findBlogValidator = (req: Request, res: Response, next: NextFunction) => {
     const blog = blogsRepository.find(req.params.id); // Ищем блог по ID
