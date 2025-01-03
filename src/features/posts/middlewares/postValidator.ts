577d033ec281cf1c103ffa14110c6ffb61998063
Bohdan Biliak <bohdan.biliak.detrox@gmail.com>
2025-01-03 10:23:46 +0000

    V1.0

--- src/features/posts/middlewares/postValidator.ts
+++ src/features/posts/middlewares/postValidator.ts
@@ -6,23 +6,31 @@ import {postsRepository} from "../postsRepository";
 import {adminMiddleware} from "../../../global_middlewares/admin-middleware";
 
 
-export const ContentValidatorMiddleware = body("content").isString().withMessage("no string").trim()
-    .isLength({min: 1, max:1000}).withMessage('more then 1000 or 0')
+export const ContentValidatorMiddleware = body("content")
+    .isString().withMessage("content must be a string")
+    .trim()
+    .isLength({ min: 1, max: 1000 }).withMessage('content length should be between 1 and 1000');
+
+
 export const blogIdValidatorMiddleware = body("blogId")
-    .isString().withMessage("no string")
+    .isString().withMessage("blogId must be a string")
     .trim()
     .custom(blogId => {
         const blog = blogsRepository.find(blogId);
-        return !!blog; // Return false if blog does not exist
+        return !!blog;
     }).withMessage("no blog with that id");
 
+
 export const TitleValidatorMiddleware = body("title")
-    .isString().withMessage("no string")
-    .isLength({ min: 1, max: 30 }).withMessage("more than 30 or 0");
+    .isString().withMessage("title must be a string")
+    .trim()
+    .isLength({ min: 1, max: 30 }).withMessage("title length should be between 1 and 30")
+    .custom(title => title.trim() !== "").withMessage("title cannot be just spaces");  // Проверка на пустые строки (только пробелы)
 
 export const ShortDescriptionValidatorMiddleware = body("shortDescription")
-    .isString().withMessage("no string")
-    .isLength({ min: 1, max: 100 }).withMessage("more than 100 or 0");
+    .isString().withMessage("shortDescription must be a string")
+    .isLength({ min: 1, max: 100 }).withMessage("shortDescription length should be between 1 and 100");
+
 export const findPostValidatorMiddleware = (req: Request<{id:string}>, res: Response, next: NextFunction) => {
     const post = postsRepository.find(req.params.id)
     if (!post) {
