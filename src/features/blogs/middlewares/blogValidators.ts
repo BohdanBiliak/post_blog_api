351195c2ae9eeffb2d22ee66ed73a5d7d82f610f
Bohdan Biliak <bohdan.biliak.detrox@gmail.com>
2025-01-03 09:58:08 +0000

    V1.0

--- src/features/blogs/middlewares/blogValidators.ts
+++ src/features/blogs/middlewares/blogValidators.ts
@@ -4,13 +4,21 @@ import {NextFunction, Response, Request} from "express";
 import {blogsRepository} from "../blogsRepository";
 import {adminMiddleware} from "../../../global_middlewares/admin-middleware";
 
-export const  nameValidator = body("name").isString().withMessage('not string')
-    .trim().isLength({min: 1, max:15}).withMessage("more than 15 or 0")
+export const  nameValidator =
+    body("name").isString().withMessage('not string')
+    .trim()
+        .isLength({min: 1, max:15}).withMessage("more than 15 or 0")
 
 export const descriptionValidator = body("description").isString().withMessage('not string')
     .trim().isLength({min: 1, max:500}).withMessage('more than 500 characters long or 0')
-export const websiteUrlValidator = body("websiteUrl").isString().withMessage('not string')
-    .trim().isLength({min:1, max:100}).withMessage('more than 100 characters long or 0')
+
+
+export const websiteUrlValidator =
+    body("websiteUrl")
+        .isString().withMessage('websiteUrl is not a valid string')
+        .isLength({ min: 1, max: 100 }).withMessage('websiteUrl length should be between 1 and 100')
+        .isURL().withMessage('websiteUrl is not a valid URL');
+
 
 export const findBlogValidator = (req: Request, res: Response, next: NextFunction) => {
     const blog = blogsRepository.find(req.params.id); // Ищем блог по ID
