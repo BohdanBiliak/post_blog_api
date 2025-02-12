87b2412aae8d4239ce297d30aada02ed5d051441
Bohdan Biliak <bohdan.biliak.detrox@gmail.com>
2025-02-12 14:21:36 +0000

    Merge remote-tracking branch 'origin/master'

--- src/features/testing/index.ts
+++ src/features/testing/index.ts
@@ -1,11 +1,12 @@
 import {Router} from "express";
-import {blogsCollection, postCollection, setDB} from "../../db/db";
+import {blogsCollection, postCollection, setDB, userCollection} from "../../db/db";
 
 export const testingRouter = Router();
 
 testingRouter.delete('/all-data', async (req, res) => {
     await blogsCollection.deleteMany({});
     await postCollection.deleteMany({});
+    await userCollection.deleteMany({});
 
     res.status(204).json({})
 })
