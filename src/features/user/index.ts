8f229affe530614a6fb8ebedda759f4fa95cb76f
Bohdan Biliak <bohdan.biliak.detrox@gmail.com>
2025-02-13 18:47:42 +0000

    Merge remote-tracking branch 'origin/master'

--- src/features/user/index.ts
+++ src/features/user/index.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import {userController} from "./controllers/userController";
 import {adminMiddleware} from "../../global_middlewares/admin-middleware";
-import {validateNewUser} from "./middlewares/userValidator";
+
 
 
 export const userRouter = express.Router();
 
-userRouter.post("/", adminMiddleware, userController.create);
+userRouter.post("/",  userController.create);
 userRouter.post("/login", userController.login);
 userRouter.get("/", userController.getAllUsers);
 userRouter.delete("/:id",adminMiddleware, userController.deleteUser);
