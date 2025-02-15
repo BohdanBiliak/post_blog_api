e4e73782691e969ca12dac8e7159396d600b3042
Bohdan Biliak <bohdan.biliak.detrox@gmail.com>
2025-02-15 21:31:00 +0000

    Merge remote-tracking branch 'origin/master'

--- src/global_middlewares/inputCheckErrorsMiddleware.ts
+++ src/global_middlewares/inputCheckErrorsMiddleware.ts
@@ -1,18 +1,19 @@
-import {Request, Response, NextFunction} from 'express';
-import {validationResult} from 'express-validator'
-import{FieldNamesType, OutputErrorsType} from "../types/output-errors-type";
+import { Request, Response, NextFunction } from "express";
+import { validationResult } from "express-validator";
+import { FieldNamesType, OutputErrorsType } from "../types/output-errors-type";
 
 export const InputCheckErrorsMiddleware = (req: Request, res: Response<OutputErrorsType>, next: NextFunction) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         console.log('Validation errors:', errors.array());
-        const errorsArray = errors.array({ onlyFirstError: true }) as { path: FieldNamesType; msg: string }[];
+        const errorsArray = errors.array() as { path: FieldNamesType; msg: string }[];
+
         return res.status(400).json({
-            errorsMessages: errorsArray.map(x => ({ message: x.msg, field: x.path  }))
+            errorsMessages: errorsArray.map(x => ({
+                message: x.msg,
+                field: x.path
+            }))
         });
     }
     next();
-}
-
-
-
+};
