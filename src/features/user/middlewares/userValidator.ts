06cb40eeaad0e063a9732d1ded6a419c1656b6c5
Bohdan Biliak <bohdan.biliak.detrox@gmail.com>
2025-02-15 22:39:41 +0000

    Merge remote-tracking branch 'origin/master'

--- src/features/user/middlewares/userValidator.ts
+++ src/features/user/middlewares/userValidator.ts
@@ -4,27 +4,22 @@ import {adminMiddleware} from "../../../global_middlewares/admin-middleware";
 
 export const LoginValidatorMiddleware = body("login")
     .exists().withMessage("Login is required")
-    .bail()
     .isString().withMessage("Login must be a string")
-    .bail()
     .trim()
     .isLength({ min: 3, max: 30 }).withMessage("Login length should be between 3 and 30");
 
 export const EmailValidatorMiddleware = body("email")
     .exists().withMessage("Email is required")
-    .bail()
     .isString().withMessage("Email must be a string")
-    .bail()
     .trim()
     .isEmail().withMessage("Invalid email format");
 
 export const PasswordValidatorMiddleware = body("password")
     .exists().withMessage("Password is required")
-    .bail()
     .isString().withMessage("Password must be a string")
-    .bail()
     .trim()
     .isLength({ min: 6, max: 50 }).withMessage("Password length should be between 6 and 50");
+
 
 
 export const validateLoginInput = (loginOrEmail: string, password: string) => {
