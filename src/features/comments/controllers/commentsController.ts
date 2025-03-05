510c4b943a8803e136e347167a35e3d7f8b24f55
Bohdan Biliak <bohdan.biliak.detrox@gmail.com>
2025-03-05 20:03:04 +0000

    Merge remote-tracking branch 'origin/master'

--- src/features/comments/controllers/commentsController.ts
+++ src/features/comments/controllers/commentsController.ts
@@ -84,7 +84,7 @@ export const commentController = {
                 return res.status(500).json({ message: "Failed to delete comment" });
             }
 
-            return res.status(200).json({ message: "Comment deleted successfully" });
+            return res.status(204).json({ message: "Comment deleted successfully" });
         } catch (error) {
             console.error("Error deleting comment:", error);
             return res.status(500).json({ message: "Internal Server Error" });
