d51d5c283ea438cf7918191a081f2772a2dc5b18
Bohdan Biliak <bohdan.biliak.detrox@gmail.com>
2025-03-13 18:48:47 +0000

    Merge remote-tracking branch 'origin/master'

---
+++ src/features/auth/email/middlewares/registrationValidation.ts
@@ -0,0 +1,43 @@
+import { body } from "express-validator";
+import {authRepository} from "../../authReposytory";
+import {InputCheckErrorsMiddleware} from "../../../../global_middlewares/inputCheckErrorsMiddleware";
+
+export const LoginValidatorMiddleware = body("login")
+    .isString().withMessage("login must be a string")
+    .trim()
+    .isLength({ min: 3, max: 20 }).withMessage("login length should be between 1 and 20")
+    .custom(async (login) => {
+        const existingUser = await authRepository.findUserByLogin(login);
+        if (existingUser) {
+            throw new Error("Login is already taken");
+        }
+        return true;
+    });
+
+
+export const EmailValidatorMiddleware = body("email")
+    .isEmail().withMessage("Invalid email format")
+    .normalizeEmail()
+    .custom(async (email) => {
+        const existingUser = await authRepository.findUserByEmail(email);
+        if (existingUser) {
+            throw new Error("Email is already taken");
+        }
+        return true;
+    });
+
+
+export const PasswordValidatorMiddleware = body("password")
+    .isString().withMessage("password must be a string")
+    .trim()
+    .isLength({min: 10, max: 20  }).withMessage("password must be at least 8 characters long")
+    //.matches(/[0-9]/).withMessage("password must contain at least one number")
+    //.matches(/[A-Za-z]/).withMessage("password must contain at least one letter");
+
+
+export const registrationValidatorMiddleware = [
+    LoginValidatorMiddleware,
+    EmailValidatorMiddleware,
+    PasswordValidatorMiddleware,
+    InputCheckErrorsMiddleware
+];
--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,12 +1,13 @@
 import {userController} from "../user/controllers/userController";
 import {Router} from 'express'
 import {authController} from "./controllers/authControllers";
+import {registrationValidatorMiddleware} from "./email/middlewares/registrationValidation";
 
 
 export const authRouter = Router()
 
 authRouter.post("/login", authController.login);
 authRouter.post("/registration-confirmation", authController.confirmRegistration)
-authRouter.post("/registration", authController.register);
+authRouter.post("/registration",registrationValidatorMiddleware, authController.register);
 authRouter.post("/registration-email-resending", authController.resendConfirmationEmail)
 authRouter.get("/me", userController.getCurrentUser);
