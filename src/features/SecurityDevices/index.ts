1afe91e4a13186019d682eee1ddcdc7b266904a3
Bohdan Biliak <arheroha@gmasil.com>
2025-04-16 13:16:26 +0100

    Merge remote-tracking branch 'origin/master'

--- src/features/SecurityDevices/index.ts
+++ src/features/SecurityDevices/index.ts
@@ -4,6 +4,6 @@ import {authMiddleware} from "./middlewares/authMiddleware";
 
 export const devicesRouter = Router();
 
-devicesRouter.get("/security/devices", authMiddleware, deviceSessionController.getSessions);
-devicesRouter.delete("/security/devices", authMiddleware, deviceSessionController.deleteAllExceptCurrent);
-devicesRouter.delete("/security/devices/:deviceId", authMiddleware, deviceSessionController.deleteDevice);
+devicesRouter.get("/devices", authMiddleware, deviceSessionController.getSessions);
+devicesRouter.delete("/devices", authMiddleware, deviceSessionController.deleteAllExceptCurrent);
+devicesRouter.delete("/devices/:deviceId", authMiddleware, deviceSessionController.deleteDevice);
